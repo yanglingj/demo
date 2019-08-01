c32282d8aafa4d9070ff2363b826ca12015c6919
yanglingjie <imv0494@dingtalk.com>
2019-08-01 11:02:54 +0800

    111

---
+++ mservice-activity-service/src/main/java/mservice/MserviceActivityServiceApplication.java
@@ -0,0 +1,13 @@
+package mservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MserviceActivityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MserviceActivityServiceApplication.class, args);
+    }
+
+}
---
+++ mservice-activity-web/src/main/java/mservice/MserviceActivityWebApplication.java
@@ -0,0 +1,13 @@
+package mservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MserviceActivityWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MserviceActivityWebApplication.class, args);
+	}
+
+}
